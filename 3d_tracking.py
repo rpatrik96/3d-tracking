02dd8cb0e64afcef19c89f269eb789b6e3ba50d7
rpatrik96 <prmedia17@gmail.com>
2018-03-31 11:15:52 +0200

    Initial commit

---
+++ 3d_tracking.py
@@ -0,0 +1,11 @@
+"""
+Created on Sat Mar 31 11:13 2018
+
+@author: Patrik Reizinger (W5PDBR)
+
+@brief:
+    This piece of code is intended to accomplish 3d position tracking
+    (orienttation tracking is a future plan) based on the Python-wrapper
+    for OpenCV, and, from a theoretical point of view, epipolar geometry
+    (with 2 webcams).
+"""
